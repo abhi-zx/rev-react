8b3182d5962230e0a3ada74415ae4a6f50a79624
Abhishek Singh <saabhiahek@gmail.com>
2022-03-09 22:41:38 +0530

    changes

---
+++ src/components/WheatherInfo.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { useState } from 'react'
+import axios from 'axios'
+const initialState = {
+    query: "",
+    data: []
+}
+const WheatherInfo = () => {
+    const [state, setState] = useState(initialState);
+    //http://api.weatherapi.com/v1/current.json?key=c22fddc0eae54538ad0183954220803&q=London&aqi=no
+    //http://api.weatherapi.com/v1/current.json?key=c22fddc0eae54538ad0183954220803
+    const show = () => {
+        // console.log(state.query)
+        axios({
+            method: "get",
+            baseURL:"http://api.weatherapi.com/v1",
+            url:"/current.json",
+            params: {
+                key:"c22fddc0eae54538ad0183954220803",
+                q:state.query
+            }
+        }).then(res => setState({...state,data:res.data}))
+            .catch(err => console.log(err))
+
+    }
+    return (
+
+        <>
+            <h1>Search For WheatherInfo of different City</h1>
+            <input type="text" value={state.query} onChange={(e) => setState({...state,query:e.target.value})}></input>
+            <button onClick={show}>Search</button>
+            {state?.data?.location && 
+            <h2>{state.data?.location?.name}</h2>}
+            {state?.data?.current && <>
+            <h5>{state?.data?.current?.condition?.text}</h5>
+            <img src={state?.data?.current?.condition?.icon} alt='wheather icon'></img>
+
+            </>}
+           
+        </>
+    )
+}
+
+export default WheatherInfo
